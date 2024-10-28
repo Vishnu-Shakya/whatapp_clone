44d6949a760562bfce6d4fec9aeffd93a87fc91c
VISHNU SHAKYA <133748244+Vishnu-Shakya@users.noreply.github.com>
2024-10-28 11:21:04 +0530

    Update socket.js

--- client/socket.js
+++ client/socket.js
@@ -2,6 +2,6 @@
 import { io } from "socket.io-client";
 
 const URL = "https://whatapp-clone.onrender.com"; // Replace with your server URL
-const socket = io(URL, { autoConnect: false , transports: ['websocket', 'polling']});
+const socket = io(URL, { autoConnect: false, transports: ["websocket"] });
 
 export default socket;
