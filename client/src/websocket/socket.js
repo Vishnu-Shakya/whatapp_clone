0d32addd5c7fe8879b565fef460a62161173ec04
VISHNU SHAKYA <133748244+Vishnu-Shakya@users.noreply.github.com>
2024-10-28 11:07:17 +0530

    cors issue fix

--- client/src/websocket/socket.js
+++ client/src/websocket/socket.js
@@ -1,6 +1,11 @@
 import { io } from 'socket.io-client';
 
-const socket = io('https://whatapp-clone.onrender.com');
+const socket = io('https://whatapp-clone.onrender.com',{
+  withCredentials: true,
+  extraHeaders: {
+    "my-custom-header": "abcd"
+  }
+});
 
 socket.on('connect', () => {
   console.log('connected to server');
