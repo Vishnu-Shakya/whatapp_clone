ef28c8e1529e513c58b71c17fabfd2dfeba762cb
VISHNU SHAKYA <133748244+Vishnu-Shakya@users.noreply.github.com>
2024-12-10 22:22:26 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -27,7 +27,8 @@ const socketfun=require('./socket/socket.js');
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: "*",
+  origin: ['https://whataapp.vercel.app', // Your original app
+    'https://whatapp-clone-qx41wlnn7-vishnu-shakyas-projects.vercel.app'],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: ["Content-Type", "my-custom-header"],  // Keep headers consistent with socket.io
   credentials: true,
